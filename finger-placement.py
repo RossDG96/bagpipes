374bbbf867244950a71b39231f123e07dff79611
RossDG96 <105515304+RossDG96@users.noreply.github.com>
2023-12-04 15:16:24 +0200

    Update finger-placement.py

--- finger-placement.py
+++ finger-placement.py
@@ -13,6 +13,10 @@ notes = [
     "High - A"
 ]
 
+time_delay = int(input("What is the time delay that should be used in seconds? "))
+iterations = int(input("How many iterations do you want to do? "))
+iterator = 0
+
 def display_random_item():
     random_item = random.choice(notes)
     print(random_item)
@@ -20,6 +24,7 @@ def display_random_item():
 def timer(seconds):
     time.sleep(seconds)
 
-while True:
+while iterator < iterations:
+    iterator += 1; 
     display_random_item()
-    timer(1)
+    timer(time_delay)
